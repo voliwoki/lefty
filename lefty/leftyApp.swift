c26b447a9d813d37ee0606fe9ef96807a6a317b0
“ninaklr” <ninakolari@gmail.com>
2026-09-15 12:46:02 +0700

    Initial Commit

---
+++ lefty/leftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  leftyApp.swift
+//  lefty
+//
+//  Created by Nina Kolari on 9/15/26.
+//
+
+import SwiftUI
+
+@main
+struct leftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
